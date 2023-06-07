728d7ca8f4fddb2b41596487a849823edfd1c29c
Kolos65 <Kolos65@users.noreply.github.com>
2023-06-07 16:32:11 +0000

    chore(release): v10.40.0

--- Package.swift
+++ Package.swift
@@ -15,12 +15,12 @@ let package = Package(
     .binaryTarget(
         name: "Realm",
         url: "https://github.com/Kolos65/RealmBinaries/releases/download/v10.40.0/Realm.xcframework.zip",
-        checksum: "0000000000000000000000000000000000000000000000000000000000000000" // Realm
+        checksum: "bf4c02ef4edb6b6889286b566bb8c577ca581b3713e05b96dd5b03a26b18bf5b" // Realm
     ),
     .binaryTarget(
         name: "RealmSwift",
         url: "https://github.com/Kolos65/RealmBinaries/releases/download/v10.40.0/RealmSwift.xcframework.zip",
-        checksum: "0000000000000000000000000000000000000000000000000000000000000000" // RealmSwift
+        checksum: "bb45492e5d3782999bf922ab2c47bd76e9ca8e0647119b391e22cff06a93f5b8" // RealmSwift
     )
   ]
 )
