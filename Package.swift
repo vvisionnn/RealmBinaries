aceb02a1e0825733f0e90842b50c406820b9ec5c
vvisionnn <vvisionnn@users.noreply.github.com>
2024-03-25 13:58:34 +0000

    chore(release): v10.49.1

--- Package.swift
+++ Package.swift
@@ -14,13 +14,13 @@ let package = Package(
   targets: [
     .binaryTarget(
         name: "Realm",
-        url: "https://github.com/vvisionnn/RealmBinaries/releases/download/v10.48.0/Realm.xcframework.zip",
-        checksum: "39152d152ef47817c8c858a4a5b84717d4eac2cf18a081d4eac73cc0a17b3964" // Realm
+        url: "https://github.com/vvisionnn/RealmBinaries/releases/download/v10.49.1/Realm.xcframework.zip",
+        checksum: "efb2fff2fe24da2baf0e4156bce980da8031b014ccb2ad2e95ba167a4c52401b" // Realm
     ),
     .binaryTarget(
         name: "RealmSwift",
-        url: "https://github.com/vvisionnn/RealmBinaries/releases/download/v10.48.0/RealmSwift.xcframework.zip",
-        checksum: "1e8540a4f6bb1154d96f3af4d28f8e1ab5db737e5d553e16491ec870fdc2922e" // RealmSwift
+        url: "https://github.com/vvisionnn/RealmBinaries/releases/download/v10.49.1/RealmSwift.xcframework.zip",
+        checksum: "2379175f6e13da55f34c08ce2f2193352c28f28437415341d3a70d2a5e6748c9" // RealmSwift
     )
   ]
 )
